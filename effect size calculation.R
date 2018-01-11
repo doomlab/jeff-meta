b714af3549ceb6e1955bb607faecb281679144d8
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-01-11 16:23:39 -0500

    Uploading files to meta

---
+++ effect size calculation.R
@@ -0,0 +1,65 @@
+##be sure to run effsize.R first
+##check the direction of the effect, pre < post
+
+##Gebler & Maercker
+d.deptavg(m2 = 49.1, sd2 = 19.5, n = 8,
+          m1 = 58.3, sd1 = 18.5)
+
+##Slavin-Spenny
+d.deptavg(m2 = 3.53, sd2 = 1.23, n = 36,
+          m1 = 3.8, sd1 = 1.39)
+
+##Smith et al 
+##need to convert this one from regression to calculate effect size 
+
+##Ulrich 
+d.deptavg(m2 = 70.68, sd2 = 20.87, n = 47,
+          m1 = 75.95, sd1 = 19.03)
+
+##EW and breast cancer baseline to 1 month 
+d.deptavg(m2 = 107.95, sd2 = 9.26, n = 26,
+          m1 = 114.83, sd1 = 10.48)
+
+##EW and breast cancer 1 month to 6 month
+d.deptavg(m2 = 114.83, sd2 = 10.48, n = 26,
+          m1 = 112.60, sd1 = 9.12)
+
+##EW kidney transplants 
+d.deptavg(m2 = 76, sd2 = 24, n = 26,
+          m1 = 44, sd1 = 7)
+
+##Hoyt and Yeater
+##need to check on this one 
+
+##Nixon & Kling 
+##If decrease in PTSD symptoms counts as PTG then we can use 
+
+##Smyth, Hockemeyer, Tulloch 
+##need to find statistics to calculate effect sizes 
+
+##Gellaitry base to one month 
+d.deptavg(m2 = 105.44, sd2 = 18.85, n = 38,
+          m1 = 109.20, sd1 = 17.51)
+d.deptavg(m2 = 106.94, sd2 = 19.31, n = 38,
+          m1 = 105.44, sd1 = 18.85)
+d.deptavg(m2 = 106.94, sd2 = 19.31, n = 38,
+          m1 = 109.56, sd1 = 19.81)
+
+##Kallay & Baban 
+d.deptavg(m2 = 56.1, sd2 = 16, n = 9,
+          m1 = 56.3, sd1 = 13.8)
+
+d.deptavg(m2 = 56.3, sd2 = 13.8, n = 9,
+          m1 = 57.7, sd1 = 15.8)
+
+d.deptavg(m2 = 20.68, sd2 = 7.56, n = 45,
+          m1 = 21.97, sd1 = 6.67)
+
+##Lancaster, Klein, Heifner 
+d.deptavg(m2 = 57.34, sd2 = 21.87, n = 45,
+          m1 = 55.90, sd1 = 21.56)
+
+##Di Blasio et al. 
+d.deptavg(m2 = 5.04, sd2 = 2.17, n = 5,
+          m1 = 3.51, sd1 = 2.21)
+
